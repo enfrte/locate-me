46852eab723153b45b13e2849c51ff353f6e086d
Leon <enfrte@gmail.com>
2017-10-19 12:57:27 +0300

    See description
    * Changed injection processing. 
    * Added logging of direct access attempts.

--- dependencies/post.php
+++ dependencies/post.php
@@ -1,37 +1,39 @@
-<?php
+<?php // process the location data from the mobile app
 
 error_reporting(0); // disable error reporting for production
+//ini_set('display_errors', 1); // display errors
 
 // some sort of security token (embedded inside the app)
-if (!isset($_POST["sec"]) || $_POST["sec"] !== "INSERT_A_SECURITY_TOKEN_HERE") {
+if (!isset($_POST["sec"]) || $_POST["sec"] !== "SECURITY_TOKEN_HERE") {
 	header("HTTP/1.0 404 Not Found"); // request for post.php did not come from the app
+	file_put_contents("404-access.log", $_SERVER['REMOTE_ADDR']."\n", FILE_APPEND | LOCK_EX); // log attempt 
 	exit; 
 }
-/* you might need this if doing some localhost testing 
+/*
 else {
-	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+	//header('Access-Control-Allow-Origin: *');
+	//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 }
 */
+
 
 $data = "";
 
 // process POST data
 foreach($_POST as $key => $value){
 	if($key !== "sec"){
-		$value = strip_tags($value);
 		if($key === "sp"){
-			$data .= intval($value).","; // sp = speed - convert null into 0 or round a float
+			$data .= (int) $value . ","; // sp = speed - convert null into 0 or round a float
 		}
-		else {
+		elseif( is_numeric($value) ){ // is_numeric will fail if injection is attempted
 			$data .= "$value,";
 		}
+		else { die('Not correct type.'); }
 	}	
 }
 
 $data = rtrim($data, ','); // remove last comma
 
-file_put_contents("data.txt", $data); 
+file_put_contents("data.txt", $data); // this data is used by google maps
 
 echo time(); // php timestamp - this goes back to the phone to tell the user that things are being updated.
-
